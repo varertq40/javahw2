ad129e357598a03c1d299bc304cea2a4282aac16
Igor <jasinski1998@gmail.com>
2023-05-08 18:54:11 +0300

    1

---
+++ task1.java
@@ -0,0 +1,37 @@
+/* Дана строка sql-запроса "select*from stedent where".
+Сформируйте часть where этого запроса, используя StringBuilder.
+Данные для фильтрации приведены ниже в виде json-строки.
+Если значение null, то параметр не должен попадать в запрос.
+Параметры для фильтрации:{"name":"Ivanov","country":"Russia",
+"city":"Moscow","age":"null"} */
+
+// {"name":"Ivanov","country":"Russia","city":"Moscow","age":"null"}
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class task1 {
+    public static void main(String[] args) {
+        Map<String, String> params1 = new HashMap<String,String>();
+        params1.put("name","Ivanov");
+        params1.put("country","Russia");
+        params1.put("city","Moscow");
+        params1.put("age",null);
+        System.out.println(getQuery(params1));
+    }
+    public static String getQuery(Map<String, String> params)
+    {
+        StringBuilder s = new StringBuilder();
+        for (Map.Entry<String,String> pair : params.entrySet())
+        {
+            if (pair.getValue() != null)
+            {
+                s.append(pair.getKey() +" = '" + pair.getValue()+"' and ");
+            }
+        }
+        s.delete(s.toString().length()-5,s.toString().length());
+        return s.toString();
+    }
+
+}
